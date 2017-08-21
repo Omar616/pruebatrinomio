c9049c5c5ba0b1c1c2aa299614ede7f951ad573f
1047330056 <1047330056@DESKTOP-M5QVPE2>
2017-08-20 20:15:59 -0500

    Calcu

---
+++ src/trinomiocuadradoperfectot/TrinomiMain.java
@@ -0,0 +1,24 @@
+
+package trinomiocuadradoperfectot;
+
+import trinomiocuadradoperfectot.Trinomiocuadradoperfecto;
+
+/**
+ * Ejercicio: Calculadora basica 
+ * Implementar la Interfaz de usuario 
+ * Implementar los metodos siguientes a través de los listeners adecuados: 
+ * - Sumar 
+ * - Restar 
+ * - Multiplicar 
+ * - Dividir
+ * 
+ *@author:  emmanuel 
+ * @version:  1.0 
+ * @date: 06-09-2015 
+ */
+public class TrinomiMain {
+ public static void main(String[] args) {
+		Trinomiocuadradoperfecto calculadora = new Trinomiocuadradoperfecto();
+		calculadora.setVisible(true);
+	}   
+}
